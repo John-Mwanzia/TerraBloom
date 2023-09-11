a411260ffc2cac023d12b3ed585967d06ad90de5
John mwanzia <johnmwanzia277@gmail.com>
2023-09-11 20:45:57 +0300

    create data object

---
+++ utils/data.ts
@@ -0,0 +1,76 @@
+const crops = [
+  {
+    name: "BANANA	FARMING",
+    Introduction:
+      "Banana is	a	perennial	crop	that does	well	in	most	tropics.	They	do	well	in	fairly	hot	and	 humid	areas	with	adequate	moisture.",
+    Ecological_requirements: {
+      Soils:
+        "Deep,	well	drained	and	fertile	soils.	Preferably,	the	soils	should	also	be	rich	in	humus",
+      PH:
+        "Slightly	acidic	to	alkaline	with	a	range	of	6	- 7.5.	Add	Agricultural	lime	if	the	soils	are	too	acidic.",
+    },
+    Varieties: {
+      Varieties:
+        "The	varieties	available	in	Kenya	include	Grand	Naine,	Williams,	Sukari	Safii,	Valery,	Grande	Naine,	 FHIA	17,	FHIA	23,	FHIA	25,	FHIA	01,	FHIA	02,	FHIA	03,	FHIA	18,	FHIA	20,	FHIA	21,	FHIA	22,	FHIA		24,	FHIA	26,	FHIA	27,	FHIA	28,	FHIA	29,	FHIA	30,	FHIA	31,	FHIA	32,	FHIA	33,	FHIA	34,	FHIA	35,	FHIA	36,	FHIA	37,	FHIA	38,	FHIA	39,	FHIA	40,	FHIA	41,	FHIA	42,	FHIA	43,	FHIA	44,	FHIA	45,	FHIA	46,	FHIA	47,	FHIA	48,	FHIA	49,	FHIA	50,	FHIA	51,	FHIA	52,	FHIA	53,	FHIA	54,	FHIA	55,	FHIA	56,	FHIA	57,	FHIA	58,	FHIA	59,	FHIA	60,	FHIA	61,	FHIA	62,	FHIA	63,	FHIA	64,	FHIA	65,	FHIA	66,	FHIA	67,	FHIA	68,	FHIA	69,	FHIA	70,	FHIA	71,	FHIA	72,	FHIA	73,	FHIA	74,	FHIA	75,	FHIA	76,	FHIA	77,	FHIA	78,	FHIA	79,	FHIA	80,	FHIA	81,	FHIA	82,	FHIA	83,	FHIA	84,	FHIA	85,	FHIA	86,	FHIA	87,	FHIA	88,	FHIA	89,	FHIA	90,	FHIA	91",
+    },
+    Land_Preparation:
+      "Select	a	place	where	bananas	had	not	been	grown	before	in	your	shamba Prepare	the	land	well	by	ploughing	and	harrowing	to	a	fine	tilth",
+    Establishment: [
+      " Make	a	hole	measuring	3	feet	by 3	feet	by	3	feet	(although	this	may	vary	with	the	 variety)",
+      " Separate	the	topsoil from	the	subsoil",
+      " With	the	topsoil,	put	1	debe	of	well	rotten	farm	yard	manure	and	a	handful	of planting	fertilizers	like	DAP	/	TSP	fertiliser.	This	accelerates the	growth	of	pseudostems.",
+      " Add	two	matchboxes	of	Meticode	or	Nemacur	nematicide	to	stop	nematodes",
+      " Mix	the	topsoil with	manure,	nematicide	and	fertilisers	well	",
+      " Fill	back	the	hole	with	the	above	mixture	then	followed	by	the	sub-soil",
+      "Choose	a	30	cm tall	seedling	and	plant	in	the	centre	of	the	hole",
+      " Mulch	the	seedling	with	dry	grass",
+      " Water	every	week.		",
+    ],
+    Establishment_Img: {
+      img: "",
+    },
+    Routine_Management_Practices: {
+      Fertilizer_and_manure_application: [
+        "Put	1	wheelbarrow	of	manure	twice	a	year	per	tree	(during	the	short	rains	and	the	 long	rains).	A	short	forked	hoe	is	used	to	incorporate	the	manure	shallowly	and	  carefully,	to	ensure	no	root	damage.",
+        " Top	dress with	100	– 125	g of	Calcium	Ammonium	Nitrate	(CAN)	around	each	stool	 each	year	in	a	15	cm	ring	around	the	stool",
+      ],
+      Weeding: [
+        "Cultivation	should	always	be	shallow	because	a	banana	plant	is	shallow	rooted",
+        "Earthing-up	of	the	stem	base	is	required	in	windy	areas.",
+      ],
+      Mulching: [
+        "Put	heavy	mulch	cover	to	suppress	unwanted	weed	growth,	retain	moisture,	and	provide	humus	for	a	good	soil	structure.	You	can	use	grass	or	banana	leaves.",
+      ],
+      Pruning_and_Stalking: [
+        "This	helps	you	to	get	bigger	and	higher	quality	bunches	as	well	as	managing	sucker	growth.	",
+        " Dead	leaves	should	be	removed	at	least	twice	a	year.	",
+        "After	harvesting,	the	pseudo-stem	should	be	cut	off	from	the	plant	at	ground	level.",
+        " Staking	of	the	fruit	bearing	pseudo-stem	should	be	done	to	prevent	breakage	caused	by	heavy	bunches.	",
+        " Stake	with	wood	by	digging	a	hole	40	- 60	cm	deep	at	the	base	of	the	stem	to	install	the	prop.	",
+        "Tie	the	bunch	to	the	prop	near	the	portion	where	the	fruit stalk	emerges	from	the	stem	using	Y-sticks.	",
+      ],
+      Banana_stool_management: [
+        " Have	only	4	trees	stands	of	different	ages	in	a	hole	i.e.	mother,	daughter,	granddaughter	and	the	great	granddaughter.	",
+        "Surplus	suckers	should	be	removed	as	early	as	possible	in	their	development	and	perhaps	used	as	planting	material.",
+      ],
+    },
+
+    Irrigation: [
+      "Water	is	needed	particularly	at	flowering.	Therefore,	in	drier	areas	supplemental irrigation	may	be	necessary	during	this time.",
+    ],
+    Windbreaks: [
+      " Bananas	are	adversely	affected	by	strong	wind.	Plant	in	sheltered	positions	and	in blocks,	rather	than	in	strips.",
+      " If	planted	in	blocks,	the	plants	provide	each	other	with	some	protection	against	the	wind.",
+    ],
+    Crop_Rotation: [
+      " Rotate	your	banana	field	after	8	years.	For	a	poorly	managed	field,	rotate	after	5	years",
+    ],
+    Pests_and_Diseases: {
+      Weevils:	 "Eat	the	stems	so	the	trees	fall.	Dig	up	and	burn	infected	trees	so weevils do	not	move	to	other	trees.",
+      Nematodes: "Attack	the	roots.	To	stop	nematodes,	use	a	Nematicide	powder	like	Meticode	or	Nemacur	at	planting,	remove	infected	stems	and	do	not	intercrop	bananas	with	maize,	cowpeas	or	sorghum.",
+      Black_Sigatoka : "	disease:	Leaves	turn	yellow	so	the	banana	trees	give	small	bunches.	Use	resistant	varieties	and	take	off	infected	leaves."
+    },
+    Maturity: "Bananas	are	ready	for	harvesting	after	about	12-15	months.	However,	maturity	indices	may	vary widely	among	varieties.	Yield	of	up	to	14 ton/acre	per	year	is	possible	under	good	management.	"
+
+  },
+];

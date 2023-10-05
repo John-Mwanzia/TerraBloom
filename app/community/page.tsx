82619e38b5e7fffdc56a0b6d29d115b953ff4ce3
John mwanzia <johnmwanzia277@gmail.com>
2023-10-05 19:37:13 +0300

    import logo

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div>
+        <div className="bg-white h-screen "></div>
+        <div></div>
+      </div>
+    </>
+  );
 }

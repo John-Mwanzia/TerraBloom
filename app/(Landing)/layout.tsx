af03bd369c7a4400f948e2c7c9c8ab1f67eb5887
John mwanzia <johnmwanzia277@gmail.com>
2023-10-06 12:09:48 +0300

    fix community layout not displaying anything

--- app/(Landing)/layout.tsx
+++ app/(Landing)/layout.tsx
@@ -1,29 +1,31 @@
 import Image from "next/image";
-const communityLayout = ({ children }) => {
-  <>
-    <div className="flex overflow-y-hidden">
-      <div className="bg-white h-screen w-[16.3rem] ">
-        <Image
-          src="community/logo.svg"
-          alt="logo"
-          className="mx-auto"
-          width={180}
-          height={133}
-        />
-      </div>
-      <div className="flex-1">
-        <div className="flex justify-end pr-3">
+const CommunityLayout = ({ children }) => {
+  return (
+    <>
+      <div className="flex overflow-y-hidden">
+        <div className="bg-white h-screen w-[16.3rem] ">
           <Image
-            src="community/avatar.svg"
-            alt="Avatar"
-            width={64}
-            height={79}
+            src="community/logo.svg"
+            alt="logo"
+            className="mx-auto"
+            width={180}
+            height={133}
           />
         </div>
-        <div className="bg-light-gray h-[100%]">{children}</div>
+        <div className="flex-1">
+          <div className="flex justify-end pr-3">
+            <Image
+              src="community/avatar.svg"
+              alt="Avatar"
+              width={64}
+              height={79}
+            />
+          </div>
+          <div className="bg-light-gray h-[100%]">{children}</div>
+        </div>
       </div>
-    </div>
-  </>;
+    </>
+  );
 };
 
-export default communityLayout;
+export default CommunityLayout;

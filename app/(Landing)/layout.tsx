8bdad2567417eea395623c421ee6dc22c90f7fd1
John mwanzia <johnmwanzia277@gmail.com>
2023-10-06 12:00:46 +0300

    code cleanup

--- app/(Landing)/layout.tsx
+++ app/(Landing)/layout.tsx
@@ -1,25 +1,29 @@
 import Image from "next/image";
-const communityLayout = ({children})=>{
-    <>
-      <div className="flex overflow-y-hidden">
-        <div className="bg-white h-screen w-[16.3rem] ">
-          <Image src="community/logo.svg" alt="logo" className="mx-auto" width={180} height={133} />
-        </div>
-        <div className="flex-1">
-          <div className="flex justify-end pr-3">
-            <Image
-              src="community/avatar.svg"
-              alt="Avatar"
-              width={64}
-              height={79}
-            />
-          </div>
-          <div className="bg-light-gray h-[100%]">
-             {children}
-          </div>
-        </div>
+const communityLayout = ({ children }) => {
+  <>
+    <div className="flex overflow-y-hidden">
+      <div className="bg-white h-screen w-[16.3rem] ">
+        <Image
+          src="community/logo.svg"
+          alt="logo"
+          className="mx-auto"
+          width={180}
+          height={133}
+        />
       </div>
-    </>
-}
+      <div className="flex-1">
+        <div className="flex justify-end pr-3">
+          <Image
+            src="community/avatar.svg"
+            alt="Avatar"
+            width={64}
+            height={79}
+          />
+        </div>
+        <div className="bg-light-gray h-[100%]">{children}</div>
+      </div>
+    </div>
+  </>;
+};
 
 export default communityLayout;

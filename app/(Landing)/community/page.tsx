5b80d2c5706f7331138a741e2eafdbc56a75a5c9
John mwanzia <johnmwanzia277@gmail.com>
2023-10-07 12:08:57 +0300

    edit home page

--- app/(Landing)/community/page.tsx
+++ app/(Landing)/community/page.tsx
@@ -2,8 +2,17 @@ import React from "react";
 
 export default function page() {
   return (
-    <div className="p-5">
-      <h1>yoow</h1>
+    <>
+    <div className="flex flex-col items-center pt-24 gap-16">
+       <div className="flex gap-16">
+          <div className="w-[29.3rem] h-[12.3rem] bg-white shadow-2xl rounded-xl"></div>
+          <div className="w-[29.3rem] h-[12.3rem] bg-white shadow-2xl rounded-xl"></div>
+       </div>
+       <div className="flex gap-16">
+          <div className="w-[29.3rem] h-[12.3rem] bg-white shadow-2xl rounded-xl"></div>
+          <div className="w-[29.3rem] h-[12.3rem] bg-white shadow-2xl rounded-xl"></div>
+       </div>
     </div>
+    </>
   );
 }

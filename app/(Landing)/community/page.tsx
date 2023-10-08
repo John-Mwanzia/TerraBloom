596cccf2ed6276b2177626ade2f6fed8391fed58
John mwanzia <johnmwanzia277@gmail.com>
2023-10-08 16:00:55 +0300

    edit homepage

--- app/(Landing)/community/page.tsx
+++ app/(Landing)/community/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <>
       <div className="flex flex-col items-center pt-24 gap-16 relative  h-[calc(100vh-5rem)]">
-        <h1 className="absolute top-6 left-72">Home</h1>
+        <h1 className="absolute top-6 left-72 text-3xl">Home</h1>
         <div className="flex gap-16 flex-wrap">
           <div className="w-[29.3rem] h-[12.3rem] bg-white shadow-2xl rounded-xl"></div>
           <div className="w-[29.3rem] h-[12.3rem] bg-white shadow-2xl rounded-xl"></div>

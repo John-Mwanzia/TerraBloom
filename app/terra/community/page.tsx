e4b49df40ef2edb1961dd63b182845f81e4c8838
John mwanzia <johnmwanzia277@gmail.com>
2023-10-18 16:25:04 +0300

    create terra community landing page

---
+++ app/terra/community/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+       <main className='h-screen bg-black/80'>
+             terraBloom community
+       </main>
+    </>
+  )
+}

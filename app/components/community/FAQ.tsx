a0634b2ed19a62d8c69a2f83230c0776822a0561
John mwanzia <johnmwanzia277@gmail.com>
2023-10-22 19:04:05 +0300

    finish up FAQ section

--- app/components/community/FAQ.tsx
+++ app/components/community/FAQ.tsx
@@ -1,14 +1,49 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function FAQ() {
   return (
-    <div className='flex justify-center'>
-      <div>
+    <div className="max-w-[80%]">
+      <h1 className="text-4xl mt-4 font-old-standard text-white mb-4">
+        Curious Corners
+      </h1>
+      <div className="flex justify-center items-center gap-36">
+        <div>
+          <h5 className="text-xl text-[#138853] font-outfit  mt-4">
+            What is TerraBloom?
+          </h5>
+          <p className="text-white max-w-xl mt-8">
+            TerraBloom is a community-driven platform that aims to connect
+            farmers, agricultural experts, and enthusiasts from all over the
+            world.
+          </p>
 
-      </div>
-      <div>
-      
+          <h5 className="text-xl text-[#138853] font-outfit mt-4 ">
+            How do I join the community?
+          </h5>
+          <p className="text-white max-w-xl mt-8">
+            You can join the community by registering on the platform. You can
+            also join the community by signing up with your Google account.
+          </p>
+
+          <h5 className="text-xl text-[#138853] font-outfit mt-4 ">
+            {" "}
+            Is my data safe on TerraBloom?
+          </h5>
+          <p className="text-white max-w-xl mt-8">
+            Yes, your data is safe on TerraBloom. We do not share your data with
+            any third-party.
+          </p>
+        </div>
+        <div>
+          <Image
+            src="/terraCommAssets/curious.svg"
+            alt="curious"
+            width={443}
+            height={459}
+          />
+        </div>
       </div>
     </div>
-  )
+  );
 }

ef8a2a9dff6f070f0fdec3eb4e8cfe3e7e72bad3
John mwanzia <johnmwanzia277@gmail.com>
2023-10-22 18:43:30 +0300

    add some new assets

--- app/components/community/FAQ.tsx
+++ app/components/community/FAQ.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function FAQ() {
   return (
-    <div>
+    <div className='flex justify-center'>
+      <div>
+
+      </div>
+      <div>
       
+      </div>
     </div>
   )
 }

2722040ab1619c06e9672ac9e5766d28034e554f
John mwanzia <johnmwanzia277@gmail.com>
2023-10-23 19:09:10 +0300

    chang some public routes

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes: [
-        '/api/trpc',
-        '/api/auth',
-        '/api/newPost',
+    publicRoutes: [  
         '/',
         '/terra/community',
     ],

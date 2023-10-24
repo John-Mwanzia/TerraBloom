980e3c97b1a2dc78e68d28c3bbb5cef7decc7df7
John mwanzia <johnmwanzia277@gmail.com>
2023-10-24 16:58:06 +0300

    create newUser page

---
+++ app/newUser/page.tsx
@@ -0,0 +1,35 @@
+import prisma from "@/modules/db";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const syncNewUser = async () => {
+  const user = await currentUser();
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id as string,
+    },
+  });
+
+  if (!existingUser) {
+    await prisma.user.create({
+      data: {
+        clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+
+  redirect("/community/Home");
+};
+
+const NewUser = async () => {
+  await syncNewUser();
+
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+        <h1 className="text-3xl">Loading...</h1>
+    </div>
+  );
+};
+export default NewUser;

6d6572b82b7d47e87f230704c2774427973d91a6
John mwanzia <johnmwanzia277@gmail.com>
2023-10-24 22:30:53 +0300

    include author when fetching posts

---
+++ prisma/migrations/20231024181951_add_avatar/migration.sql
@@ -0,0 +1,85 @@
+/*
+  Warnings:
+
+  - The primary key for the `Comment` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Like` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Post` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - A unique constraint covering the columns `[clerkId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `clerkId` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_postId_fkey";
+
+-- AlterTable
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "postId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Comment_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Comment_id_seq";
+
+-- AlterTable
+ALTER TABLE "Like" DROP CONSTRAINT "Like_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "postId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Like_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Like_id_seq";
+
+-- AlterTable
+ALTER TABLE "Post" DROP CONSTRAINT "Post_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Post_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Post_id_seq";
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "avatarUrl" TEXT,
+ADD COLUMN     "clerkId" TEXT NOT NULL,
+ALTER COLUMN "password" DROP NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Introduction" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Introduction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Announcement" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Announcement_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Introduction_authorId_key" ON "Introduction"("authorId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkId_key" ON "User"("clerkId");
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Introduction" ADD CONSTRAINT "Introduction_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Announcement" ADD CONSTRAINT "Announcement_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

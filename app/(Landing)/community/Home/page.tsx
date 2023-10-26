764d25e638dc12f5808b4c4a1d1c4987b8a143c5
John mwanzia <johnmwanzia277@gmail.com>
2023-10-26 19:56:46 +0300

    reposition my spinner

--- app/(Landing)/community/Home/page.tsx
+++ app/(Landing)/community/Home/page.tsx
@@ -60,7 +60,7 @@ export default function Page() {
         <div className="flex-1 overflow-y-auto pb-8">
           <div className="flex flex-col items-center pt-8 gap-16 relative pb-24">
             {loading ? (
-              <div>
+              <div className="absolute top-72 ">
                 <Spinner size="lg" />
               </div>
             ) : (

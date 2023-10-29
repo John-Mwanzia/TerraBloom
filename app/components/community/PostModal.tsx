71392a78dd6195ad431fee14a1357ea2b4af167c
John mwanzia <johnmwanzia277@gmail.com>
2023-10-29 21:48:14 +0300

    code cleanup

--- app/components/community/PostModal.tsx
+++ app/components/community/PostModal.tsx
@@ -1,96 +1,3 @@
-// import React from "react";
-// import ModalForm from "./ModalForm";
-// import Image from "next/image";
-
-// interface PostModalProps {
-//   setShowModal: (value: boolean) => void;
-// }
-
-// export default function PostModal({ setShowModal }: PostModalProps) {
-//   return (
-//     <div className="bg-black/80 w-screen h-screen fixed  top-0 right-0">
-//       <div>
-//         <div className="bg-white w-[50rem] h-[40rem] rounded-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-//           <div className="flex justify-between px-4 pt-6 border-b-2 pb-2">
-//             <h1 className="text-3xl">Create Post</h1>
-//             <div className="flex gap-4 items-center">
-//               <div>
-//                 <button type="button" onClick={() => setShowModal(false)}>
-//                   <Image
-//                     src="/community/close.svg"
-//                     alt="close"
-//                     width={22}
-//                     height={18}
-//                   />
-//                 </button>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="mt-6">
-//             <ModalForm />
-//           </div>
-//           <div className="flex justify-between absolute bottom-4 px-6 border-t-2  w-full pt-4">
-//             <div className="flex gap-6">
-//               <div>
-//                 <button>
-//                   <Image
-//                     src="/community/attach.svg"
-//                     alt="close"
-//                     width={22}
-//                     height={18}
-//                   />
-//                 </button>
-//               </div>
-//               <div>
-//                 <button>
-//                   <Image
-//                     src="/community/video.svg"
-//                     alt="close"
-//                     width={22}
-//                     height={18}
-//                   />
-//                 </button>
-//               </div>
-//               <div>
-//                 <button>
-//                   <Image
-//                     src="/community/imageUpload.svg"
-//                     alt="close"
-//                     width={22}
-//                     height={18}
-//                   />
-//                 </button>
-//               </div>
-//               <div>
-//                 <button>
-//                   <Image
-//                     src="/community/gif.svg"
-//                     alt="close"
-//                     width={22}
-//                     height={18}
-//                   />
-//                 </button>
-//               </div>
-//               <div>
-//                 <button>
-//                   <Image
-//                     src="/community/emoji.svg"
-//                     alt="close"
-//                     width={22}
-//                     height={18}
-//                   />
-//                 </button>
-//               </div>
-//             </div>
-
-//             <button className="bg-[#0E9AA9] rounded px-3 py-2">Post</button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 // PostModal.js
 import React, { useContext, useState } from "react";
 import ModalForm from "./ModalForm";
@@ -165,7 +72,10 @@ export default function PostModal({ setShowModal }) {
   };
 
   return (
-    <div className="bg-black/80 w-screen h-screen fixed  top-0 right-0" style={{ zIndex: 1000000 }}>
+    <div
+      className="bg-black/80 w-screen h-screen fixed  top-0 right-0"
+      style={{ zIndex: 1000000 }}
+    >
       <div>
         <div className="bg-white w-[23rem] h-[70%] xl:w-[50rem] md:w-[90%] xl:h-[40rem] rounded-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <div className="flex justify-between px-4 pt-6 border-b-2 pb-2">
@@ -233,8 +143,6 @@ export default function PostModal({ setShowModal }) {
               </div>
               {/* Add buttons for other actions here */}
             </div>
-
-           
           </div>
         </div>
       </div>

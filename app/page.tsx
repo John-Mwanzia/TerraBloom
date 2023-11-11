35120a6c975d23349c93265abe221f100c7d9af0
John mwanzia <johnmwanzia277@gmail.com>
2023-11-11 11:44:56 +0300

    create about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from './components/HomeLanding/HeroSection'
+import AboutSection from './components/HomeLanding/AboutSection'
 
 export default function page() {
   return (
-    <div className='bg-light-gray/40  flex justify-center'>
+    <div className='bg-light-gray/40  flex justify-center items-center flex-col'>
         <HeroSection />
+        <AboutSection />
     </div>
   )
 }

3fe63521997cd331b6721380a059ebfc565cdf1a
John mwanzia <johnmwanzia277@gmail.com>
2023-11-15 19:06:47 +0300

    sm screen responsiveness

--- app/components/HomeLanding/AboutSection.tsx
+++ app/components/HomeLanding/AboutSection.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function AboutSection() {
   return (
-    <div className="bg-light-pink max-w-[90%] 2xl:max-w-[80%] mt-12 rounded-md flex justify-center px-4 lg:px-24 gap-10 xl:gap-32 2xl:gap-48 mb-24  pb-24 lg:pb-32 pt-12 lg:pr-12">
+    <div className="bg-light-pink max-w-[90%] 2xl:max-w-[80%] mt-12 rounded-md flex flex-col md:flex-row justify-center sm:px-24 md:px-4 lg:px-24 gap-10 xl:gap-32 2xl:gap-48 mb-24  pb-24 lg:pb-32 pt-12 lg:pr-12">
       <div>
         <Image
           src="./landing/about.svg"
@@ -13,7 +13,7 @@ export default function AboutSection() {
         />
       </div>
       <div>
-        <h1 className="font-outfit text-4xl lg:text-5xl  max-w-xl">
+        <h1 className="font-outfit   sm:text-4xl lg:text-5xl  max-w-xl">
           Find Out a Little More About Us
         </h1>
         <div className="max-w-lg mt-10 lg:mt-20">

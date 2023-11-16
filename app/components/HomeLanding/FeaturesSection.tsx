ca21cffe4b7059e531c1339241f6eb1114d59267
John mwanzia <johnmwanzia277@gmail.com>
2023-11-16 15:11:59 +0300

    create some feature cards

--- app/components/HomeLanding/FeaturesSection.tsx
+++ app/components/HomeLanding/FeaturesSection.tsx
@@ -2,9 +2,28 @@ import React from "react";
 
 export default function FeaturesSection() {
   return (
-    <div>
-      <div className="text-center">
+    <div className="max-w-[90%] 2xl:max-w-[80%] mb-24">
+      <div className="text-center font-outfit  text-3xl sm:text-4xl   max-w-4xl">
         <h1>Powerful Features to Revolutionize Your Farming Experience</h1>
+      </div>
+      <div className="mt-16">
+        <div className="flex justify-center gap-24">
+          <div className="bg-[#89898a42] transform skew-x-45 scale-y-100 text-center px-12 py-20 rounded-3xl">
+            <h3 className="mb-12 ml-24 text-2xl font-outfit text-start  max-w-[15rem]">Real-Time Weather Updates</h3>
+            <p className="max-w-xs">
+              Stay informed with real-time weather forecasts and alerts specific
+              to your region
+            </p>
+          </div>
+          <div className="bg-[#89898a42] text-center px-12 py-20 rounded-3xl">
+            <h3 className="mb-12 text-start ml-24 text-2xl font-outfit max-w-[15rem] ">Real-Time Weather Updates,</h3>
+            <p className="max-w-xs font-outfit">
+              Stay informed with real-time weather forecasts and alerts specific
+              to your region
+            </p>
+          </div>
+        </div>
+        <div></div>
       </div>
     </div>
   );

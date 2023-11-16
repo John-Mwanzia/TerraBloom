652da452362697118707c8324c0f79788f48e8cc
John mwanzia <johnmwanzia277@gmail.com>
2023-11-16 13:05:53 +0300

    create features section

---
+++ app/components/HomeLanding/FeaturesSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function FeaturesSection() {
+  return (
+    <div>
+      <div className="text-center">
+        <h1>Powerful Features to Revolutionize Your Farming Experience</h1>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from './components/HomeLanding/HeroSection'
 import AboutSection from './components/HomeLanding/AboutSection'
+import FeaturesSection from './components/HomeLanding/FeaturesSection'
 
 export default function page() {
   return (
     <div className='bg-light-gray/40  flex justify-center items-center flex-col'>
         <HeroSection />
         <AboutSection />
+        <FeaturesSection />
     </div>
   )
 }

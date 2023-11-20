a0691f66f0b3d210b2cba906eb439180ca553c13
John mwanzia <johnmwanzia277@gmail.com>
2023-11-20 14:49:26 +0300

    edit testimonial section

--- app/components/HomeLanding/Testimonial.tsx
+++ app/components/HomeLanding/Testimonial.tsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function Testimonial() {
   return (
-    <div className=' bg-[#FFF] overflow-x-hidden w-screen flex  justify-center overflow-y-hidden'>
-      <div>
-        <h1 className='font-outfit  text-3xl sm:text-4xl lg:text-5xl'>
-        What Our Farmers Say
+    <div className=" bg-[#FFF] overflow-x-hidden w-screen  overflow-y-hidden mb-16">
+      <div className=" flex justify-center flex-col items-center">
+        <h1 className="font-outfit  text-3xl sm:text-4xl lg:text-5xl mt-16">
+          What Our Farmers Say
         </h1>
+        <p className="text-center max-w-3xl mt-10">
+          Don't just take our word for it. Hear from farmers who have
+          experienced the transformative power of our agricultural web
+          applicationa
+        </p>
       </div>
-      <div className='flex justify-center'>
-         <div></div>
-         <div></div>
+      <div className="flex justify-center mt-10">
+        <div className="relative">
+          <Image
+            src="./landing/testimonial/sunshine.svg"
+            alt="testimonial sunshine"
+            width={536}
+            height={668}
+          />
+          <Image
+            src="./landing/testimonial/farmer2.svg"
+            alt="testimonial farmer"
+            width={264}
+            height={194}
+            className="absolute top-40 -left-36"
+          />
+          <div className="bg-[#C3CFD9] w-28 h-28 absolute -right-16 rounded-3xl top-48">
+
+          </div>
+        </div>
+        <div></div>
       </div>
     </div>
-  )
+  );
 }

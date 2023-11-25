c7888b6b837a4cdf061e2385950c2e8884ff5745
John mwanzia <johnmwanzia277@gmail.com>
2023-11-25 12:19:20 +0300

    mobile  screen testimonial section  responsiveness

--- app/components/HomeLanding/Testimonial.tsx
+++ app/components/HomeLanding/Testimonial.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Testimonial() {
   return (
     <div className=" bg-[#FFF] overflow-x-hidden w-screen  overflow-y-hidden mb-16 pb-12 flex flex-col items-center justify-center ">
-      <div className="xl:max-w-[90%] 2xl:max-w-[80%] sm:px-24 xl:px-36">
+      <div className="xl:max-w-[90%] 2xl:max-w-[80%] px-10 sm:px-24 xl:px-36">
         <div className=" flex justify-center flex-col items-center">
           <h1 className="font-outfit  text-3xl sm:text-4xl lg:text-5xl mt-16">
             What Our Farmers Say
@@ -15,8 +15,8 @@ export default function Testimonial() {
             application
           </p>
         </div>
-        <div className="flex justify-center mt-10 sm:gap-16 xl:gap-32 items-center">
-          <div className="relative">
+        <div className="flex flex-col sm:flex-row justify-center mt-10 sm:gap-16 xl:gap-32 items-center">
+          <div className="relative px-10 sm:px-0">
             <Image
               src="./landing/testimonial/sunshine.svg"
               alt="testimonial sunshine"
@@ -28,9 +28,9 @@ export default function Testimonial() {
               alt="testimonial farmer"
               width={264}
               height={194}
-              className="absolute sm:w-36 md:w-40 xl:w-48 2xl:w-64 sm:top-20  xl:top-36 2xl:top-40 sm:-left-16 xl:-left-28 2xl:-left-36"
+              className="absolute w-32 sm:w-36 md:w-40 xl:w-48 2xl:w-64 top-24 sm:top-20  xl:top-36 2xl:top-40 -left-2 sm:-left-16 xl:-left-28 2xl:-left-36"
             />
-            <div className="bg-[#C3CFD9] w-20 h-20 absolute -right-10 rounded-3xl sm:top-24 md:top-28  xl:top-44 2xl:top-56"></div>
+            <div className="bg-[#C3CFD9] w-20 h-20 absolute -right-2 sm:-right-10 rounded-3xl top-28 sm:top-24 md:top-28  xl:top-44 2xl:top-56"></div>
           </div>
           <div>
             <div className="mb-12 flex  justify-end">
@@ -39,7 +39,7 @@ export default function Testimonial() {
                 alt="quotes"
                 width={98}
                 height={98}
-                className=" sm:w-10 md:w-16  xl:w-24"
+                className="w-10 md:w-16  xl:w-24 mt-6 sm:mt-0"
               />
             </div>
             <div className="mb-12">

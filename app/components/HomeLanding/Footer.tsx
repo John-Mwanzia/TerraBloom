4675911352a27653b4337414592ffda8aefca994
John mwanzia <johnmwanzia277@gmail.com>
2023-11-26 19:38:51 +0300

    fix footer grid background issues

--- app/components/HomeLanding/Footer.tsx
+++ app/components/HomeLanding/Footer.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+
 
 export default function Footer() {
   return (
-    <div className='bg-[#30324A] w-screen h-[800px]'>
-      
+    <div className="bg-[#30324A] w-screen h-[1056px] bg-footer-texture bg-cover  bg-[center_-50rem] ">
+      <div>
+        
+      </div>
     </div>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,9 +17,7 @@ const config: Config = {
          
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "footer-texture": "url('/landing/footer/grid.svg')",
       },
       fontFamily: {
 

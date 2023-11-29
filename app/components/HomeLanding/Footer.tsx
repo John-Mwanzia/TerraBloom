581fa13d61735bf40cad6ae96716c67484625556
John mwanzia <johnmwanzia277@gmail.com>
2023-11-29 15:20:32 +0300

    fully responsive landing page

--- app/components/HomeLanding/Footer.tsx
+++ app/components/HomeLanding/Footer.tsx
@@ -4,15 +4,15 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="bg-[#30324A] w-screen mt-16 max-h-[69rem] bg-footer-texture bg-cover sm:bg-[center_-12rem] md:bg-[center_-19rem] xl:bg-[center_-28rem] 2xl:bg-[center_-50rem] overflow-hidden ">
+    <div className="bg-[#30324A] w-screen mt-16  sm:h-[69rem] bg-footer-texture bg-cover bg-[center_-19rem] sm:bg-[center_-12rem] md:bg-[center_-19rem] xl:bg-[center_-28rem] 2xl:bg-[center_-50rem] overflow-hidden ">
       <div className="mt-12">
         <h1 className="text-white font-outfit text-4xl text-center">
           Get in Touch
         </h1>
       </div>
       <div className="mt-12 relative ">
-        <div className="relative sm:top-56 md:top-10 md:-left-12 xl:-left-10 xl:top-16 2xl:left-0 2xl:top-0">
-          <div className="relative -top-16 left-10">
+        <div className="relative top-48  sm:top-56 md:top-10 -left-10 sm:left-0 md:-left-12 xl:-left-10 xl:top-16 2xl:left-0 2xl:top-0">
+          <div className="relative -top-12 sm:-top-16 left-10">
             <button>
               <Image
                 src="/landing/footer/quickToast.svg"
@@ -22,7 +22,7 @@ export default function Footer() {
               />
             </button>
           </div>
-          <div className="relative -top-36 left-36">
+          <div className="relative -top-32 sm:-top-36 left-32 sm:left-36">
             <button>
               <Image
                 src="/landing/footer/blog.svg"
@@ -32,7 +32,7 @@ export default function Footer() {
               />
             </button>
           </div>
-          <div className="relative -top-48 left-64">
+          <div className="relative -top-48 left-52 sm:left-64">
             <button>
               <Image
                 src="/landing/footer/features.svg"
@@ -42,7 +42,7 @@ export default function Footer() {
               />
             </button>
           </div>
-          <div className="relative -top-60 left-[22rem]">
+          <div className="relative -top-40 sm:-top-60 left-14 sm:left-[22rem]">
             <button>
               <Image
                 src="/landing/footer/about.svg"
@@ -52,7 +52,7 @@ export default function Footer() {
               />
             </button>
           </div>
-          <div className="relative -top-72 left-[29rem]">
+          <div className="relative -top-56 sm:-top-72 left-36 sm:left-[29rem]">
             <button>
               <Image
                 src="/landing/footer/pricing.svg"
@@ -63,7 +63,7 @@ export default function Footer() {
             </button>
           </div>
 
-          <div className="relative -top-[22rem] left-[35rem]">
+          <div className="relative -top-72 sm:-top-[22rem] left-56 sm:left-[35rem]">
             <button>
               <Image
                 src="/landing/footer/Home.svg"
@@ -74,18 +74,18 @@ export default function Footer() {
             </button>
           </div>
         </div>
-        <div className="absolute sm:top-0 md:top-0 sm:-right-10 xl:right-44 2xl:right-72">
+        <div className="absolute top-0 sm:top-0 md:top-0 -right-8 sm:-right-10 xl:right-44 2xl:right-72">
           <div>
             <Image
               src="/landing/footer/cardModal.svg"
               width={600}
               height={500}
               alt="blog"
-              className="sm:w-[32rem]  md:w-[27rem] xl:w-[37rem]"
+              className="w-[20rem] sm:w-[32rem]  md:w-[27rem] xl:w-[37rem]"
             />
           </div>
         </div>
-        <div className="absolute sm:top-[41rem] md:top-[29rem] xl:top-48 sm:right-[25rem]  md:right-16 xl:right-24 2xl:right-52">
+        <div className="absolute top-[52rem] sm:top-[41rem] md:top-[29rem] xl:top-48 right-8 sm:right-[25rem]  md:right-16 xl:right-24 2xl:right-52">
           <div className="relative -left-36 top-12">
             <Image
               src="/landing/footer/socialMedia.svg"
@@ -127,7 +127,7 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <div className="relative sm:-top-80 sm:left-10 md:-top-40 md:left-16 xl:-top-56 xl:left-20">
+        <div className="relative -top-32 sm:-top-80 sm:left-10 md:-top-40 md:left-16 xl:-top-56 xl:left-20">
           <div className="relative -top-6">
             <Image
               src="/landing/footer/terms.svg"
@@ -145,7 +145,7 @@ export default function Footer() {
             />
           </div>
         </div>
-        <div className="relative sm:left-[20rem] md:left-[24rem] xl:left-[34rem] -top-96">
+        <div className="relative sm:left-[20rem] md:left-[24rem] xl:left-[34rem] bottom-0 sm:-top-96">
           <Image
           src="/landing/footer/copyright.svg"
           width={400}

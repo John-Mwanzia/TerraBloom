2c5d0a212605ae09603f13af93ea2f1a3504aa4c
John mwanzia <johnmwanzia277@gmail.com>
2024-01-31 19:56:09 +0300

    edit crop details page

--- app/crop-details/page.tsx
+++ app/crop-details/page.tsx
@@ -1,19 +1,21 @@
 import Image from "next/image";
 import React from "react";
+import Nav from "../components/HomeLanding/Nav";
 
 export default function page() {
   return (
-    <div>
-      <div>
-        {/* logo */}
-        <Image
-          src="/bloomCommAssets/Logo.svg"
-          alt="logo"
-          width={100}
-          height={100}
-        />
+    <div className="flex justify-center items-center flex-col overflow-x-hidden">
+      <Nav />
+      <div className=" w-[95%] sm:w-[90%] xl:w-[90%] 2xl:w-[80%] flex justify-between ">
+        <div>
+          <h1 className="text-3xl font-bold">
+            All Crops
+          </h1>
+        </div>
+        <div className="bg-[#32CD32] pl-3 w-[75%] px-4 py-4">
+          Crops
+        </div>
       </div>
-      <h1>crop details</h1>
     </div>
   );
 }

8e66c736a012dd906cac267690d0d2fbb5875ead
John mwanzia <johnmwanzia277@gmail.com>
2024-02-22 20:30:36 +0300

    add custom fonts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
 };
 
 module.exports = nextConfig;

2154cb666b95011fe4c24d801b8fb8170fa9ff06
John Mwanzia <johnmwanzia277@gmail.com>
2024-07-18 23:03:28 +0300

    feat: add aos

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react";
 
 export default function loading() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen bg-black w-screen items-center justify-center">
       <Loader className="h-16 w-16 animate-spin text-primary" />
     </div>
   );

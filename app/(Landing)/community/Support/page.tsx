8b1cdffba670deb6562759eb3c63f0bf189f8a1a
John Mwanzia <johnmwanzia277@gmail.com>
2024-08-05 19:28:48 +0300

    feat: edit support page

--- app/(Landing)/community/Support/page.tsx
+++ app/(Landing)/community/Support/page.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      support
+     <div className="flex justify-between border-l border-t bg-white px-4 pb-4 pt-3 dark:bg-[#2B2E33]/50 dark:text-white">
+          <h1 className="text-3xl">Support</h1>
+        </div>
+        <div>
+          
+        </div>
     </div>
   )
 }
